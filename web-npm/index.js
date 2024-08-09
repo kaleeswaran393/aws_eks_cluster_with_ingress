a7b4f01b59abd3ddba46ac8d011febe4a6c430c9
rvasuki <32475673+rvasuki@users.noreply.github.com>
2024-08-09 19:16:19 +0800

    Update index.js with the redis server

--- web-npm/index.js
+++ web-npm/index.js
@@ -3,17 +3,19 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient({
-    host: "redis-server",
+    //host: "redis",
+    host: "172.20.52.63", 
     port: 6379
 });
 client.set('visits', 0);
 
-app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
+   res.send('Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
-});
+ });
+
 
 app.listen(8081, () => {
   console.log('Listening on port 8081');
