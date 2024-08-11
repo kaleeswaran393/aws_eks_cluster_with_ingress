29b8dbdbc8c074fc6221eef44bc6efc382e812a2
Vasuki Rajagopal <rvasuki84@gmail.com>
2024-08-11 07:05:45 +0000

    Update file provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   profile    = var.profile
   region = "ap-southeast-1"
+  AWS_ACCESS_KEY_ID={$AWS_ACCESS_KEY_ID}
+  AWS_SECRET_ACCESS_KEY={$AWS_SECRET_ACCESS_KEY}
   default_tags {
     tags = {
       provisioned_mode = "terraform"
