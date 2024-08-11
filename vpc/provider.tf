06afdabb78050cb150304caedac7e4e3cda04c7f
Vasuki Rajagopal <rvasuki84@gmail.com>
2024-08-11 07:26:11 +0000

    Update 2 files
    - /vpc/provider.tf
    - /.gitlab-ci.yml

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  profile    = var.profile
+  profile = var.profile
   region = "ap-southeast-1"
-  access_key = {$AWS_ACCESS_KEY_ID}
-  secret_key = {$AWS_SECRET_ACCESS_KEY}
+
   default_tags {
     tags = {
       provisioned_mode = "terraform"
     }
   }
 }
+
+  access_key={$AWS_ACCESS_KEY_ID}
+  secret_key={$AWS_SECRET_ACCESS_KEY}
