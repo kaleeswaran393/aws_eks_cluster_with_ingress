0050826a776adb55a389aa44fcdf7a6c6fac0a80
rvasuki <32475673+rvasuki@users.noreply.github.com>
2024-09-03 20:43:17 +0530

    added proifile for vpc

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  name   = "${var.PROFILE}_${var.prefix_name}"
+  name   = "${var.profile}_${var.prefix_name}"
   region = "ap-southeast-1"
   tags_all = {
-    Environment = var.PROFILE
+    Environment = var.profile
   }
 }
